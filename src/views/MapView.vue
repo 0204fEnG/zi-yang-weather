3fe1244b83eec0e680dccda8f4d94fa16078c574
feng <794761652@qq.com>
2024-10-25 19:34:34 +0800

    2024/10/25/19:34

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -8,6 +8,20 @@ import AMapLoader from '@amap/amap-jsapi-loader'
 
 export default {
   name: 'MapView',
+  data () {
+    return {
+      key: '70624df561ac6289fc2859eb6e1582ca',
+      securityJsCode: '845c0f04a314d5a4fce02831ced87da3',
+      currentPosition: {
+        latitude: null,
+        longitude: null
+      },
+      map: null
+    }
+  },
+  created () {
+    // this.getLocation()
+  },
   mounted () {
     this.initAMap()
   },
@@ -15,22 +29,59 @@ export default {
     this.map?.destroy()
   },
   methods: {
+    // getLocation () {
+    //   console.log('hello')
+    //   if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition((position) => {
+    //       console.log('world')
+    //       this.currentPosition.latitude = position.coords.latitude
+    //       this.currentPosition.longitude = position.coords.longitude
+    //       this.initAMap()
+    //     }, (err) => {
+    //       console.warn('ERROR(' + err.code + '): ' + err.message)
+    //     }, {
+    //       enableHighAccuracy: true,
+    //       timeout: 10000,
+    //       maximumAge: 0
+    //     })
+    //   } else {
+    //     console.log('Geolocation is not supported by this browser.')
+    //   }
+    // },
     initAMap () {
       window._AMapSecurityConfig = {
-        securityJsCode: '845c0f04a314d5a4fce02831ced87da3'
+        securityJsCode: this.securityJsCode
       }
       AMapLoader.load({
-        key: '70624df561ac6289fc2859eb6e1582ca', // 申请好的Web端开发者Key，首次调用 load 时必填
+        key: this.key, // 申请好的Web端开发者Key，首次调用 load 时必填
         version: '2.0', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-        plugins: ['AMap.Scale'] // 需要使用的的插件列表，如比例尺'AMap.Scale'，支持添加多个如：['...','...']
+        plugins: ['AMap.Scale', 'AMap.Geolocation'] // 需要使用的的插件列表，如比例尺'AMap.Scale'，支持添加多个如：['...','...']
       })
         .then((AMap) => {
+          const mapGeoLocation = new AMap.Geolocation({
+            position: 'RB',
+            showCircle: false,
+            panToLocation: true,
+            zoomToAccuracy: true
+          })
+          mapGeoLocation.getCurrentPosition((status, result) => {
+            if (status === 'complete') {
+              this.currentPosition.latitude = result.position.lat
+              this.currentPosition.longitude = result.position.lng
+            } else {
+              console.log('获取经纬度错误！')
+            }
+          })
           this.map = new AMap.Map('map-view', {
             // 设置地图容器id
             viewMode: '3D', // 是否为3D地图模式
-            zoom: 11, // 初始化地图级别
-            center: [116.397428, 39.90923] // 初始化地图中心点位置
+            zoom: 16, // 初始化地图级别
+            center: [this.currentPosition.longitude, this.currentPosition.latitude] // 初始化地图中心点位置
           })
+          const scale = new AMap.Scale()
+          this.map.addControl(scale)
+          scale.show()
+          this.map.addControl(mapGeoLocation)
         })
         .catch((e) => {
           console.log(e)
