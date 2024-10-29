ea280a181679ece156057b72511169178fcd0636
feng <794761652@qq.com>
2024-10-29 22:50:13 +0800

    2024/10/29/22:50

--- src/components/WeatherCountry.vue
+++ src/components/WeatherCountry.vue
@@ -1,5 +1,10 @@
 <template>
-<div class="weather-country">{{text}}</div>
+<div class="weather-country">
+  <!-- <div class="country">岳麓区</div> -->
+  <div class="temperature">17</div>
+  <div class="weather">阴</div>
+  <div class="air-qualit">良好</div>
+</div>
 </template>
 
 <script>
@@ -9,19 +14,41 @@ export default {
     return {
       text: 'hello!'
     }
-  },
-  created () {
-    setTimeout(() => {
-      this.text += 'world!'
-    }, 2000)
   }
 }
 </script>
 
 <style scoped>
 .weather-country{
+  /* position: relative; */
+  display:flex;
+  flex-direction: row;
+  flex-wrap: wrap;
     width: 100%;
-    height: 200px;
   background-color: rgb(208, 113, 50);
+}
+/* .country{
+  text-align: center;
+  font-size: 30px;
+  line-height: 30px;
+  width: 100%;
+  height: 5vh;
+  background-color: brown;
+} */
+.temperature{
+  width: 100%;
+  height: 10vh;
+background-color: blue;
+}
+.weather{
+  width: 50%;
+  height: 2vh;
+background-color: rgb(188, 188, 101);
+}
+.air-qualit{
+  width: 50%;
+  height: 2vh;
+  line-height: 2vh;
+background-color: rgb(97, 100, 125)
 }
 </style>
--- src/views/WeatherDetail.vue
+++ src/views/WeatherDetail.vue
@@ -1,5 +1,10 @@
 <template>
 <div id="weather-detail">
+  <div class="country">
+    <div>岳麓区</div>
+    <button class="left">&lt;</button>
+    <button class="right">&gt;</button>
+  </div>
   <WeatherCountry></WeatherCountry>
   <Weather24Hours></Weather24Hours>
   <Weather7Days></Weather7Days>
@@ -8,10 +13,10 @@
 </template>
 
 <script>
-import WeatherCountry from '@/components/WeatherCountry.vue'
-import Weather24Hours from '@/components/Weather24Hours.vue'
-import Weather7Days from '@/components/Weather7Days.vue'
-import CorrelationIndex from '@/components/CorrelationIndex.vue'
+import WeatherCountry from '@/components/WeatherCountry'
+import Weather24Hours from '@/components/Weather24Hours'
+import Weather7Days from '@/components/Weather7Days'
+import CorrelationIndex from '@/components/CorrelationIndex'
 export default {
   components: {
     WeatherCountry,
@@ -33,5 +38,34 @@ export default {
     width: 100%;
     height: 1400px;
     background-color:rgb(80, 111, 101);
+}
+.country{
+  position: sticky;
+  text-align: center;
+  line-height: 5vh;
+  font-size:30px;
+  top:63px;
+  width: 100%;
+  height: 5vh;
+  background-color: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(7px);
+}
+.left{
+  position: absolute;
+  border-radius: 12px;
+  border-color: white;
+  left: 20px;
+  width: 24px;
+  height: 24px;
+  top:calc(50% - 12px);
+}
+.right{
+  position: absolute;
+  border-color:white;
+  border-radius: 12px;
+  width: 24px;
+  height: 24px;
+  right: 20px;
+  top:calc(50% - 12px);
 }
 </style>
